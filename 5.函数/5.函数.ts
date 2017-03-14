dddff7a149d01c8eb091dcae0ec19cd9c6758e43
yindi <17744559519@163.com>
2017-03-14 15:36:55 +0800

    this 关键字

--- 5.函数/5.函数.ts
+++ 5.函数/5.函数.ts
@@ -46,9 +46,73 @@ let myAdd3:(baseValue:number, increment:number)=>number =
 /** 只要参数类型是匹配的就可以,不在乎参数名是否正确*/
 /** 函数的返回值类型是必须的!!!!如果没有返回值那么也要用void表示,不能留空!!!*/
 /*
-* 第二部分是返回值类型.
-* 我们在函数和返回值类型之前使用(=>)箭头符号
-*/
+ * 第二部分是返回值类型.
+ * 我们在函数和返回值类型之前使用(=>)箭头符号
+ */
 
 /** 5.2.3 推断类型 */
+//在赋值语句的一边指定了类型但是另一边没有类型的话，
+// TypeScript编译器会自动识别出类型
+// 这叫做按上下文归类
+let myAdd4 = function (x:number, y:number):number {
+    return x + y;
+};
+let myAdd5:(baseValue:number, increment:number) => number =
+    function (x, y) {
+        return x + y;
+    };
 
+/** 5.3可选参数与默认参数 */
+/** 传递给一个函数的参数个数必须与函数期望的参数个数一致。*/
+function buildName(firstName:string, lastName:string) {
+    return firstName + ' ' + lastName;
+}
+// let result1=buildName('bob');     //error too few parameters
+// let result2=buildName('bob','adams','Sr.'); //error too many parameters
+let result3 = buildName('blb', 'adams');
+
+/** 在参数名字后面加入 '?'  来表示可选参数功能
+ *  可选参数必须跟在必须参数后面
+ * */
+
+function buildName2(firstName:string, lastName?:string) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
+}
+
+let result4 = buildName2("Bob");  // works correctly now
+// let result5 = buildName2("Bob", "Adams", "Sr.");  // error, too many parameters
+let result6 = buildName2("Bob", "Adams");  // ah, just right
+
+/** 默认值 */
+function buildName3(firstName:string, lastName = "Smith") {
+    return firstName + " " + lastName;
+}
+
+let result7 = buildName3("Bob");                  // works correctly now, returns "Bob Smith"
+let result8 = buildName3("Bob", undefined);       // still works, also returns "Bob Smith"
+// let result9 = buildName3("Bob", "Adams", "Sr.");  // error, too many parameters
+let result10 = buildName3("Bob", "Adams");         // ah, just right
+let result11 = buildName3("Bob", null);     // still works, also returns "Bob Smith"
+
+/** 可选参数与默认值参数 !!!都是可选的!!!!  他们共享参数类型*/
+
+/** 与普通可选参数不同的是，带默认值的参数不需要放在必须参数的后面。 如果带默认值的参数出现在必须参数前面，用户必须明确的传入 undefined值来获得默认值*/
+
+/** 5.4 剩余参数 */
+function buildName4(firstName:string, ...restOfName:string[]) {
+    return firstName + " " + restOfName.join(" ");
+}
+
+let employeeName = buildName4("Joseph", "Samuel", "Lucas", "MacKinzie");
+function buildName5(firstName:string, ...restOfName:string[]) {
+    return firstName + " " + restOfName.join(" ");
+}
+
+let buildNameFun:(fname:string, ...rest:string[]) => string = buildName5;
+
+
+/** 5.5 this */
+/** 5.5.1 this 和 箭头函数 */

490f3bc86a8c14f84e8a03d7a82c1e6f5f68faaa
yindi <17744559519@163.com>
2017-03-15 16:42:09 +0800

    提交

--- 11.Symbol/11.Symbol.ts
+++ 11.Symbol/11.Symbol.ts
@@ -5,4 +5,9 @@
 //自ECMAscript 2015起,symbol成为了一种新的原生类型,就像number,string一样
 //symbol 是通过过 Symbol构造函数创建的
 let sym1 = Symbol();
-
+interface a{
+    readonly x:number;
+    readonly y:number;
+}
+console.log(22222);
+let sym2=Symbol;

7d6dd907ac4e21ed48f35423a072a19a8971e258
yindi <17744559519@163.com>
2017-03-16 21:42:41 +0800

    提交html

---
+++ 13.module/LettersOnlyValidator.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by yindi on 17/3/16.
+ */
+import { StringValidator } from "./Validation";
+
+const lettersRegexp = /^[A-Za-z]+$/;
+
+export class LettersOnlyValidator implements StringValidator {
+    isAcceptable(s: string) {
+        return lettersRegexp.test(s);
+    }
+}
---
+++ 13.module/Validation.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by yindi on 17/3/16.
+ */
+export interface StringValidator {
+    isAcceptable(s: string): boolean;
+}
